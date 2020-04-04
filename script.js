aa0a7e791bec09f381f6a33b332e4ecc26ada110
Geogolf <62524875+Geogolf@users.noreply.github.com>
2020-04-04 08:30:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,20 +18,26 @@ var game = {
   i4costBase: [1e6, 1e12, 1e24, 1e48, 1e96, 1e192],
   acount: [0, 0, 0, 0, 0, 0, 0],
   acondition: [0, 0, 0, 0, 0, 0, 0],
-  s1cost: [1e12, 1e19, "Max"],
+  s1cost: [1e12, 1e24, 1e48, "Max"],
   s1count: 0,
+  pptotal: 0,
   pp: 0,
   pcount: 0,
   refundAmount: 0,
   u1cost: [1, 4, 9, 16, 25, "Max"],
   u1count: 0,
-  u2cost: [2, 5, 10, "Max"],
+  u1max: 5,
+  u1push: [490, 8100, 121000, 1.69e6, 2.25e7],
+  u2cost: [2, 5, 10, 19, 30, "Max"],
   u2count: 0,
-  u3cost: [10, 15, "Max"],
+  u2max: 5,
+  u2push: [600, 10500, 210000, 3.15e6, 5.25e7],
+  u3cost: [10, 20, 35, 55, 80, "Max"],
   u3count: 0,
+  u3max: 5,
   c1in: 0,
   c1count: 0,
-  c1goal: [1e9, 1e10],
+  c1goal: [1e9, 1e10, 1e11, 1e12, 1e13, 1e18, 1e19, 1e20, 1e21, 1e22],
   ainterval: 41,
   inTab: 1,
 };
@@ -47,28 +53,29 @@ var aenabled = ["Disabled", "Enabled"];
 
 function unlock() {
   if (game.incrementPrePrestige >= 0) {
-    document.getElementById("unlockReqI").innerHTML = e(game.i2cost - game.incrementPrePrestige);
-    document.getElementById("unlockI").innerHTML = "Increment";
+    document.getElementById("unlockReqI").innerHTML = e(10 - game.incrementPrePrestige);
+    document.getElementById("unlockI").innerHTML = "Increment 2";
   }
-  if (game.incrementPrePrestige >= game.i2cost) {
+  if (game.incrementPrePrestige >= 10) {
     document.getElementById("increment2").style.display = "inline";
-    document.getElementById("unlockReqI").innerHTML = e(game.i3cost - game.incrementPrePrestige);
+    document.getElementById("unlockReqI").innerHTML = e(1000 - game.incrementPrePrestige);
+    document.getElementById("unlockI").innerHTML = "Increment 3";
   }
-  if (game.incrementPrePrestige >= game.i3cost) {
+  if (game.incrementPrePrestige >= 1000) {
     document.getElementById("increment3").style.display = "inline";
-    document.getElementById("unlockReqI").innerHTML = e(100000 - game.incrementPrePrestige);
-    document.getElementById("unlockI").innerHTML = "Automation";
+    document.getElementById("unlockReqI").innerHTML = e(2500 - game.incrementPrePrestige);
+    document.getElementById("unlockI").innerHTML = "Automation + Automate 1";
   }
-  if (game.incrementPrePrestige >= 100000) {
+  if (game.incrementPrePrestige >= 2500) {
     document.getElementById("tabAutomation").style.display = "inline";
     document.getElementById("automate1").style.display = "inline";
-    document.getElementById("unlockReqI").innerHTML = e(game.i4cost[game.i4count] - game.incrementPrePrestige);
-    document.getElementById("unlockI").innerHTML = "Increment";
+    document.getElementById("unlockReqI").innerHTML = e(1e6 - game.incrementPrePrestige);
+    document.getElementById("unlockI").innerHTML = "Increment 4";
   }
-  if (game.incrementPrePrestige >= game.i4cost[game.i4count]) {
+  if (game.incrementPrePrestige >= 1e6) {
     document.getElementById("increment4").style.display = "inline";
     document.getElementById("unlockReqI").innerHTML = e(1e8 - game.incrementPrePrestige);
-    document.getElementById("unlockI").innerHTML = "Automate";
+    document.getElementById("unlockI").innerHTML = "Automate 2";
   }
   if (game.incrementPrePrestige >= 1e8) {
     document.getElementById("automate2").style.display = "inline";
@@ -78,7 +85,7 @@ function unlock() {
   if (game.incrementPrePrestige >= 1e9) {
     document.getElementById("tabPrestige").style.display = "inline";
     document.getElementById("unlockReqI").innerHTML = e(1e12 - game.incrementPrePrestige);
-    document.getElementById("unlockI").innerHTML = "Scaling";
+    document.getElementById("unlockI").innerHTML = "Scaling + Scaling 1";
   }
   if (game.incrementPrePrestige >= 1e12) {
     document.getElementById("unlockReqI").innerHTML = "WIP";
@@ -94,25 +101,26 @@ function unlock() {
     document.getElementById("tabPrestige").style.display = "inline";
     document.getElementById("upgrade1").style.display = "inline";
     document.getElementById("textu1").style.display = "inline";
-    document.getElementById("unlockReqP").innerHTML = 2 - game.pp;
+    document.getElementById("unlockReqU").innerHTML = 2 - game.pp;
+    document.getElementById("unlockU").innerHTML = "Upgrade 2";
   }
   if (game.pcount >= 2) {
     document.getElementById("upgrade2").style.display = "inline";
     document.getElementById("textu2").style.display = "inline";
-    document.getElementById("unlockReqP").innerHTML = 5 - game.pp;
-    document.getElementById("unlockP").innerHTML = "Challenges";
+    document.getElementById("unlockReqU").innerHTML = 5 - game.pp;
+    document.getElementById("unlockU").innerHTML = "Challenges + Challenge 1";
   }
   if (game.pcount >= 5) {
     document.getElementById("challenges").style.display = "inline";
     document.getElementById("challenge1").style.display = "inline";
-    document.getElementById("unlockReqP").innerHTML = 10 - game.pp;
-    document.getElementById("unlockP").innerHTML = "Upgrade";
+    document.getElementById("unlockReqU").innerHTML = 10 - game.pp;
+    document.getElementById("unlockU").innerHTML = "Upgrade 3";
   }
   if (game.pcount >= 10) {
     document.getElementById("upgrade3").style.display = "inline";
     document.getElementById("textu3").style.display = "inline";
-    document.getElementById("unlockReqP").innerHTML = "WIP";
-    document.getElementById("unlockP").innerHTML = "WIP";
+    document.getElementById("unlockReqU").innerHTML = "WIP";
+    document.getElementById("unlockU").innerHTML = "WIP";
   }
   if (game.c1in == 1) {
     document.getElementById("ctext").style.display = "inline";
@@ -142,11 +150,17 @@ function refresh() {
   document.getElementById("i2cost").innerHTML = e(game.i2cost);
   document.getElementById("i3").innerHTML = game.i3;
   document.getElementById("i3cost").innerHTML = e(game.i3cost);
-  document.getElementById("i4").innerHTML = e(game.i4);
+  document.getElementById("i4").innerHTML = d(game.i4);
   document.getElementById("i4cost").innerHTML = e(game.i4cost[game.i4count]);
   document.getElementById("s1cost").innerHTML = e(game.s1cost[game.s1count]);
   document.getElementById("s1count").innerHTML = game.s1count;
   document.getElementById("refundAmount").innerHTML = game.refundAmount;
+  if (game.increment >= 1e9) {
+    document.getElementById("pponprestige").innerHTML = 1;
+  }
+  else {
+    document.getElementById("pponprestige").innerHTML = 0;
+  }
   document.getElementById("u1cost").innerHTML = game.u1cost[game.u1count];
   document.getElementById("u1count").innerHTML = game.u1count;
   document.getElementById("u2cost").innerHTML = game.u2cost[game.u2count];
@@ -154,7 +168,11 @@ function refresh() {
   document.getElementById("u3cost").innerHTML = game.u3cost[game.u3count];
   document.getElementById("u3count").innerHTML = game.u3count;
   document.getElementById("c1count").innerHTML = game.c1count;
-  document.getElementById("s1cost").innerHTML = e(game.s1cost[game.s1count]);
+  document.getElementById("c1reward").innerHTML = game.ainterval;
+  document.getElementById("u1max").innerHTML = game.u1max;
+  document.getElementById("u2max").innerHTML = game.u2max;
+  document.getElementById("u3max").innerHTML = game.u3max;
+  document.getElementById("c3count").innerHTML = game.c3count;
 }
 function hide() {
   document.getElementById("prestigePointsTop").style.display = "none";
@@ -169,7 +187,7 @@ function hide() {
   document.getElementById("increment6").style.display = "none";
   document.getElementById("increment7").style.display = "none";
   document.getElementById("tab2").style.display = "none";
-  document.getElementById("tab3").style.display = "none";
+  document.getElementById("tab4").style.display = "none";
   document.getElementById("automate1").style.display = "none";
   document.getElementById("automate2").style.display = "none";
   document.getElementById("automate3").style.display = "none";
@@ -177,7 +195,7 @@ function hide() {
   document.getElementById("automate5").style.display = "none";
   document.getElementById("automate6").style.display = "none";
   document.getElementById("automate7").style.display = "none";
-  document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab5").style.display = "none";
   document.getElementById("scale1").style.display = "none";
   document.getElementById("scale2").style.display = "none";
   document.getElementById("scale3").style.display = "none";
@@ -194,18 +212,18 @@ function hide() {
   document.getElementById("upgrade3").style.display = "none";
   document.getElementById("textu3").style.display = "none";
   document.getElementById("tab8").style.display = "none";
-  document.getElementById("challenge1").style.display = "none"
-  document.getElementById("challenge2").style.display = "none"
-  document.getElementById("challenge3").style.display = "none"
-  document.getElementById("challenge4").style.display = "none"
+  document.getElementById("challenge1").style.display = "none";
+  document.getElementById("challenge2").style.display = "none";
+  document.getElementById("challenge3").style.display = "none";
+  document.getElementById("challenge4").style.display = "none";
   document.getElementById("ctext").style.display = "none";
   document.getElementById("ccomplete").style.display = "none";
 }
 function tab(n) {
   document.getElementById("tab1").style.display = "none";
   document.getElementById("tab2").style.display = "none";
-  document.getElementById("tab3").style.display = "none";
   document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab5").style.display = "none";
   document.getElementById("tab6").style.display = "none";
   document.getElementById("tab7").style.display = "none";
   document.getElementById("tab8").style.display = "none";
@@ -228,6 +246,14 @@ function e(num) {
   }
   else {
     return Number(num).toFixed();
+  }
+}
+function d(num) {
+  if (typeof num === "string") {
+    return num;
+  }
+  else {
+    return Number(num).toFixed(1);
   }
 }
 
@@ -259,20 +285,26 @@ function reset() {
   game.inTab = 1;
   game.acount = [0, 0, 0, 0, 0, 0, 0];
   game.acondition = [0, 0, 0, 0, 0, 0, 0];
-  game.s1cost = [1e12, 1e19, "Max"];
+  game.s1cost = [1e12, 1e24, 1e48, "Max"];
   game.s1count = 0;
+  game.pptotal = 0;
   game.pp = 0;
   game.pcount = 0;
   game.refundAmount = 0;
   game.u1cost = [1, 4, 9, 16, 25, "Max"];
   game.u1count = 0;
-  game.u2cost = [2, 5, 10, "Max"];
+  game.u1max = 5;
+  game.u1push = [490, 8100, 121000, 1.69e6, 2.25e7];
+  game.u2cost = [2, 5, 10, 19, 30, "Max"];
   game.u2count = 0;
-  game.u3cost = [10, 15, "Max"];
+  game.u2max = 5;
+  game.u2push = [600, 10500, 210000, 3.15e6, 5.25e7];
+  game.u3cost = [10, 20, 35, 55, 80, "Max"];
   game.u3count = 0;
+  game.u3max = 5;
   game.c1in = 0;
   game.c1count = 0;
-  game.c1goal = [1e9, 1e10];
+  game.c1goal = [1e9, 1e10, 1e11, 1e12, 1e13, 1e18, 1e19, 1e20, 1e21, 1e22];
   game.ainterval = 41;
   game.inTab = 1;
   tab(1);
@@ -286,14 +318,16 @@ function reset() {
 function resetPrestige() {
   game.acount = [0, 0, 0, 0, 0, 0, 0];
   game.acondition = [0, 0, 0, 0, 0, 0, 0];
-  game.incrementPrePrestige = 0;
-  game.increment = 0;
-  game.i2count = 0;
-  game.i3count = 0;
-  game.i4count = 0;
-  unlock();
-  display();
-  refresh();
+  setTimeout(function() {
+    game.incrementPrePrestige = 0;
+    game.increment = 0;
+    game.i2count = 0;
+    game.i3count = 0;
+    game.i4count = 0;
+    unlock();
+    display();
+    refresh();
+  }, 10);
 }
 function load() {
   let data = JSON.parse(localStorage.getItem("gameFile"));
@@ -302,6 +336,7 @@ function load() {
   }
 }
 function loadGame(data) {
+  game = {}
   if (typeof data.incrementHighest != "undefined") game.incrementHighest = data.incrementHighest;
   if (typeof data.incrementTotal != "undefined") game.incrementTotal = data.incrementTotal;
   if (typeof data.incrementPrePrestige != "undefined") game.incrementPrePrestige = data.incrementPrePrestige;
@@ -323,15 +358,22 @@ function loadGame(data) {
   if (typeof data.acondition != "undefined") game.acondition = data.acondition;
   if (typeof data.s1cost != "undefined") game.s1cost = data.s1cost;
   if (typeof data.s1count != "undefined") game.s1count = data.s1count;
+  if (typeof data.pptotal != "undefined") game.pptotal = data.pptotal;
   if (typeof data.pp != "undefined") game.pp = data.pp;
   if (typeof data.pcount != "undefined") game.pcount = data.pcount;
   if (typeof data.refundAmount != "undefined") game.refundAmount = data.refundAmount;
   if (typeof data.u1cost != "undefined") game.u1cost = data.u1cost;
   if (typeof data.u1count != "undefined") game.u1count = data.u1count;
+  if (typeof data.u1max != "undefined") game.u1max = data.u1max;
+  if (typeof data.u1push != "undefined") game.u1push = data.u1push;
   if (typeof data.u2cost != "undefined") game.u2cost = data.u2cost;
   if (typeof data.u2count != "undefined") game.u2count = data.u2count;
+  if (typeof data.u2max != "undefined") game.u2max = data.u2max;
+  if (typeof data.u2push != "undefined") game.u2push = data.u2push;
   if (typeof data.u3cost != "undefined") game.u3cost = data.u3cost;
   if (typeof data.u3count != "undefined") game.u3count = data.u3count;
+  if (typeof data.u3max != "undefined") game.u3max = data.u3max;
+  if (typeof data.u3push != "undefined") game.u3push = data.u3push;
   if (typeof data.c1in != "undefined") game.c1in = data.c1in;
   if (typeof data.c1count != "undefined") game.c1count = data.c1count;
   if (typeof data.c1goal != "undefined") game.c1goal = data.c1goal;
@@ -385,11 +427,11 @@ function importGame() {
     loadGame(iGame);
   }
 }
-setInterval(function() {
+/* setInterval(function() {
   if (game.increment > game.incrementPrePrestige || game.pp > game.pcount) {
     reset();
   }
-}, 1000);
+}, 1000); */
 
 function increment(i) {
   if (i == 1) {
@@ -403,18 +445,21 @@ function increment(i) {
   if (i == 2 && game.increment >= game.i2cost) {
     game.increment -= game.i2cost;
     game.i2count++;
+    unlock();
     display();
     refresh();
   }
   if (i == 3 && game.increment >= game.i3cost) {
     game.increment -= game.i3cost;
     game.i3count++;
+    unlock();
     display();
     refresh();
   }
   if (i == 4 && game.increment >= game.i4cost[game.i4count]) {
     game.increment -= game.i4cost[game.i4count];
     game.i4count++;
+    unlock();
     display();
     refresh();
   }
@@ -439,49 +484,28 @@ function scale(s) {
 }
 
 function upgrade(u) {
-  if (u == 1 && game.pp >= game.u1cost[game.u1count] && game.u1count < 5 && game.incrementPrePrestige == 0) {
+  if (u == 1 && game.pp >= game.u1cost[game.u1count] && game.u1count < game.u1max && game.i2count == 0) {
     game.pp -= game.u1cost[game.u1count];
     game.refundAmount += game.u1cost[game.u1count];
     game.u1count++;
     game.i2quotient -= 0.01;
-    display();
   }
-  if (u == 2 && game.pp >= game.u2cost[game.u2count] && game.u2count < 5 && game.incrementPrePrestige == 0) {
+  if (u == 2 && game.pp >= game.u2cost[game.u2count] && game.u2count < game.u2max && game.i3count == 0) {
     game.pp -= game.u2cost[game.u2count];
     game.refundAmount += game.u2cost[game.u2count];
     game.u2count++;
-    display();
   }
-  if (u == 3 && game.pp >= game.u3cost[game.u3count] && game.u3count < 5 && game.incrementPrePrestige == 0) {
+  if (u == 3 && game.pp >= game.u3cost[game.u3count] && game.u3count < game.u3max && game.i4count == 0) {
     game.pp -= game.u3cost[game.u3count];
     game.refundAmount += game.u3cost[game.u3count];
     game.u3count++;
-    display();
     for (let i = 0; i < game.i4cost.length; i++) {
       game.i4cost[i] = game.i4costBase[i] * (3 ** (i + game.u3count));
     }
   }
+  display();
   refresh();
 }
-function challenge(c) {
-  if (c == 1 && game.c1in == 0 && game.c1count < 2) {
-    game.c1in = 1;
-    resetPrestige();
-    hide();
-    unlock();
-    display();
-    refresh();
-    tab(8);
-  }
-}
-function ccomplete(cc) {
-  if (game.c1in == 1 && game.increment >= game.c1goal[game.c1count]) {
-    game.c1in = 1;
-    game.c1count++;
-    game.ainterval -= 4;
-  }
-}
-
 function refund() {
   if (game.u1count > 0) {
     game.u1count--;
@@ -504,7 +528,6 @@ function refund() {
   else {
     game.i2quotient = 1.11;
     game.i4cost = [1e6, 1e12, 1e24, 1e48, 1e96, 1e192];
-    resetPrestige();
     hide();
     unlock();
     display();
@@ -516,21 +539,48 @@ function prestige() {
   if (game.increment >= 1e9) {
     resetPrestige();
     game.pp++;
+    game.pptotal++;
     game.pcount++;
     document.getElementById("prestigePointsTop").style.display = "inline";
     document.getElementById("unlockI").innerHTML = "Increment";
-    hide();
-    tab(7);
     document.getElementById("a1enabled").innerHTML = "Disabled";
     document.getElementById("a2enabled").innerHTML = "Disabled";
-    document.getElementById("a3enabled").innerHTML = "Disabled"
+    document.getElementById("a3enabled").innerHTML = "Disabled";
+    hide();
+    tab(7);
     unlock();
     display();
     refresh();
   }
 }
+
+function challenge(c) {
+  if (c == 1 && game.c1in == 0 && game.c1count < 10) {
+    game.c1in = 1;
+    for (let i =  1; i <= 7; i++) {
+      document.getElementById("a" + i + "enabled").innerHTML = "Disabled"; 
+    }
+    resetPrestige();
+    hide();
+    unlock();
+    display();
+    refresh();
+    tab(8);
+  }
+}
+function ccomplete(cc) {
+  if (game.c1in == 1 && game.increment >= game.c1goal[game.c1count]) {
+    game.c1in = 0;
+    game.c1count++;
+    game.ainterval -= 4;
+    exit();
+  }
+}
 function exit() {
   game.c1in = 0;
+  for (let i =  1; i <= 7; i++) {
+    document.getElementById("a" + i + "enabled").innerHTML = "Disabled"; 
+  }
   resetPrestige();
   hide();
   unlock();
@@ -542,6 +592,7 @@ function exit() {
 hide();
 tab(1);
 load();
-unlock();
-refresh();
 loadAutomation();
+unlock();
+display();
+refresh();
